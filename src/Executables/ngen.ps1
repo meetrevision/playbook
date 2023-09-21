d6290f6203cd8ec5fccac527ce1db0581cffc084
h3r0 <50238852+melo936@users.noreply.github.com>
2023-09-21 12:07:04 +0400

    improve PowerShell startup performance

---
+++ src/Executables/ngen.ps1
@@ -0,0 +1,10 @@
+# Credits to https://stackoverflow.com/users/9898643/theo
+$env:PATH = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+[AppDomain]::CurrentDomain.GetAssemblies() | ForEach-Object {
+    $path = $_.Location
+    if ($path) { 
+        $name = Split-Path $path -Leaf
+        Write-Host -ForegroundColor Yellow "`r`nRunning ngen on '$name'"
+        ngen.exe install $path /nologo
+    }
+}
