9a5ebcc5fb2d4dbd6a5ecd8908b5243519d5aedc
h3r0 <50238852+melo936@users.noreply.github.com>
2023-09-24 18:51:32 +0400

    #26

--- src/Executables/EDGE.cmd
+++ src/Executables/EDGE.cmd
@@ -13,13 +13,57 @@ for /D %%I in ("%ProgramFiles(x86)%\Microsoft\Edge\Application\*") do (
     )
 )
 
+
 msiexec /X{2BFF39DC-EFF0-355C-80CD-41D847013784} /qn /norestart
 
-set "value=MicrosoftEdgeAutoLaunch_.*"
-set "path=HKCU\Software\Microsoft\Windows\CurrentVersion\Run"
 
-for /f "tokens=1,*" %%A in ('reg query "%path%" /f "Microsoft" ^| findstr /R "%value%"') do (
-    reg delete "%path%" /v "%%A" /f
+::leftovers
+
+for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
+	reg query "HKU\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul 2>&1
+	if not errorlevel 1 (
+		for /f "usebackq tokens=2* delims= " %%b in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "AppData" 2^>^&1 ^| findstr /r /x /c:".*AppData[ ]*REG_SZ[ ].*"`) do (
+			echo del "%%c\Microsoft\Internet Explorer\Quick Launch\Microsoft Edge.lnk" /q /f
+			del "%%c\Microsoft\Internet Explorer\Quick Launch\Microsoft Edge.lnk" /q /f
+			echo del "%%c\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk" /q /f
+			del "%%c\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk" /q /f
+		)
+	)
 )
 
-del /q /f "%userprofile%\Desktop\Microsoft Edge.lnk" >NUL 2>nul
+for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
+	reg query "HKU\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul 2>&1
+	if not errorlevel 1 (
+		CALL :USERREG "%%a"
+	)
+)
+
+ for /f "usebackq delims=" %%e in (`reg query "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" ^| findstr /i /r /c:"Microsoft[ ]*Edge" /c:"msedge"`) do reg delete "%%e" /f
+
+ for /f "usebackq delims=" %%a in (`dir /b /a:d "%SystemDrive%\Users" ^| findstr /v /i /x /c:"Public" /c:"Default User" /c:"All Users"`) do (
+	echo del /q /f "%homeDrive%\Users\%%a\Desktop\Microsoft Edge.lnk"
+	del /q /f "%homeDrive%\Users\%%a\Desktop\Microsoft Edge.lnk"
+
+	echo rmdir /q /s "%homeDrive%\Users\%%a\AppData\Local\Microsoft\Edge"
+	rmdir /q /s "%homeDrive%\Users\%%a\AppData\Local\Microsoft\Edge"
+
+	echo rmdir /q /s "%homeDrive%\Users\%%a\AppData\Local\Microsoft\EdgeUpdate"
+	rmdir /q /s "%homeDrive%\Users\%%a\AppData\Local\Microsoft\EdgeUpdate"
+	
+	echo del /q /f "%homeDrive%\Users\%%a\Desktop\Microsoft Edge.lnk"
+	del /q /f "%homeDrive%\Users\%%a\Desktop\Microsoft Edge.lnk"
+	echo del /q /f "%homeDrive%\Users\%%a\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories\Internet Explorer.lnk"
+	del /q /f "%homeDrive%\Users\%%a\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories\Internet Explorer.lnk"
+)
+
+for /f "usebackq tokens=1* delims=\" %%A in (`schtasks /query /fo list ^| findstr /c:"\MicrosoftEdge"`) do (
+	schtasks /delete /tn "%%B" /f
+)
+
+exit /b 0
+
+:USERREG
+for /f "usebackq tokens=1 delims= " %%e in (`reg query "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" ^| findstr /i /c:"MicrosoftEdge" /c:"msedge"`) do (
+	echo reg delete "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "%%e" /f
+	reg delete "HKU\%~1\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "%%e" /f
+)
