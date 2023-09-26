117547d5dc60bde7a7900a29f9c57fed22310cb4
h3r0 <50238852+melo936@users.noreply.github.com>
2023-09-26 15:52:44 +0400

    use EnableSF instead

--- src/Executables/FINALIZE.cmd
+++ src/Executables/FINALIZE.cmd
@@ -23,13 +23,7 @@ echo Configuring Superfetch for HDD...
 for /f %%i in ('PowerShell -NonInteractive -NoLogo -NoP -C "(Get-PhysicalDisk -SerialNumber (Get-Disk -Number (Get-Partition -DriveLetter $env:SystemDrive.Substring(0, 1)).DiskNumber).SerialNumber.TrimStart()).MediaType"') do set "hardDrive=%%i"
 
 if "%hardDrive%"=="HDD" (
-  reg add "HKLM\SYSTEM\ControlSet001\Control\Class\{71a27cdd-812a-11d0-bec7-08002be2092f}" /v "LowerFilters" /t REG_MULTI_SZ /d "fvevol\0iorate\0rdyboost" /f >NUL 2>nul
-  reg add "HKLM\SYSTEM\ControlSet001\Services\rdyboost" /v "Start" /t REG_DWORD /d "0" /f >NUL 2>nul
-  reg add "HKLM\SYSTEM\ControlSet001\Services\SysMain" /v "Start" /t REG_DWORD /d "2" /f >NUL 2>nul
-  reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\Memory Management\PrefetchParameters" /v "EnablePrefetcher" /t REG_DWORD /d "3" /f >NUL 2>nul
-  reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\Memory Management\PrefetchParameters" /v "EnableSuperfetch" /t REG_DWORD /d "3" /f >NUL 2>nul
-  reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\EMDMgmt" /v "GroupPolicyDisallowCaches" /f >NUL 2>nul
-  reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\EMDMgmt" /v "AllowNewCachesByDefault" /f >NUL 2>nul
+  @start /b "" "%programfiles(x86)%\Revision Tool\data\flutter_assets\additionals\MinSudo.exe" --NoLogo --TrustedInstaller "%programfiles(x86)%\Revision Tool\data\flutter_assets\additionals\EnableSF.bat"
 )
 
 echo Configuring memory...
