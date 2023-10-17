e7f4c72174f572129352af768a8a7c4f9e3cdf89
h3r0 <50238852+melo936@users.noreply.github.com>
2023-10-17 10:26:51 +0000

    move tweaks from config.yml to finalize.cmd

--- src/Executables/FINALIZE.cmd
+++ src/Executables/FINALIZE.cmd
@@ -3,6 +3,22 @@ set version=23.10
 for /f "tokens=2 delims==" %%i in ('wmic os get BuildNumber /value ^| find "="') do set "build=%%i"
 if %build% gtr 19045 ( set "w11=true" )
 
+
+:: Update Health Tools
+msiexec /X{43D501A5-E5E3-46EC-8F33-9E15D2A2CBD5} /qn /norestart >NUL 2>nul
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\UpdateHealthTools" /f >NUL 2>nul
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\rempl" /f >NUL 2>nul
+reg delete "HKLM\SOFTWARE\Microsoft\CloudManagedUpdate" /f >NUL 2>nul
+rmdir /s /q "%ProgramW6432%\\Microsoft Update Health Tools" >NUL 2>nul
+
+:: PC Health Check
+msiexec /X{804A0628-543B-4984-896C-F58BF6A54832} /qn /norestart >NUL 2>nul
+rmdir /s /q "%ProgramW6432%\\PCHealthCheck" >NUL 2>nul
+
+:: Windows Installation Assistant
+"%ProgramFiles(x86)%\WindowsInstallationAssistant\Windows10UpgraderApp.exe" /SunValley /ForceUninstall >NUL 2>nul
+rmdir /s /q "%ProgramFiles(x86)%\WindowsInstallationAssistant" >NUL 2>nul
+
 if not defined w11 (
 	bcdedit /set description "ReviOS 10 %version%" >NUL 2>nul
   reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "Model"  /t REG_SZ /d "ReviOS 10 %version%" /f >NUL 2>nul
@@ -16,7 +32,69 @@ if not defined w11 (
   PowerShell -NonInteractive -NoLogo -NoP -C "& { $Cbs = \"$env:SystemRoot\\SystemApps\\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\"; $Manifest = Join-Path $Cbs 'appxmanifest.xml'; takeown /a /f $Manifest; icacls $Manifest /grant Administrators:F; $ReviManifest = Join-Path $Cbs \"appxmanifest.xml.revi\"; if (!(Test-Path $ReviManifest)) { Copy-Item -Path $Manifest -Destination $ReviManifest -Force; Remove-Item $Manifest -Force }; [xml]$xml = Get-Content -Path \"$Cbs\\appxmanifest.xml.revi\" -Raw; $applicationNode = $xml.Package.Applications.Application | Where-Object { $_.Id -eq 'WebExperienceHost' }; if ($applicationNode -ne $null) { $xml.Package.Applications.RemoveChild($applicationNode) } $xml.Save($Manifest) }"
 )
 
+PowerShell -NonInteractive -NoLogo -NoP -C "Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod" >NUL 2>nul
+
+echo Configuring power settings
+powercfg /hibernate off >NUL 2>nul
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 >NUL 2>nul
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 3ff9831b-6f80-4830-8178-736cd4229e7b >NUL 2>nul
+powercfg -changename 3ff9831b-6f80-4830-8178-736cd4229e7b "Revision - Ultra Performance" "Windows's Ultimate Performance with additional changes." >NUL 2>nul
+powercfg -s 3ff9831b-6f80-4830-8178-736cd4229e7b >NUL 2>nul
+powercfg -setacvalueindex scheme_current sub_processor PERFINCPOL 2 >NUL 2>nul
+powercfg -setacvalueindex scheme_current sub_processor PERFDECPOL 1 >NUL 2>nul
+powercfg -setacvalueindex scheme_current sub_processor PERFINCTHRESHOLD 10 >NUL 2>nul
+powercfg -setacvalueindex scheme_current sub_processor PERFDECTHRESHOLD 8 >NUL 2>nul
+powercfg /setactive scheme_current >NUL 2>nul
+
 PowerShell -NonInteractive -NoLogo -NoP -C "& {$cpu = Get-CimInstance Win32_Processor; $cpuName = $cpu.Name; if ($cpu.Manufacturer -eq 'GenuineIntel') { if ($cpuName.Substring(0, 2) -eq 'In') { Write-Host 'Detected Intel CPU older than 10th generation.' } else { $cpuGen = [int]($cpuName.Substring(0, 2)); if ($cpuGen -gt 11) { Write-Host 'Optimizing Revision''s Ultra powerplan for 12th generation or later Intel CPUs'; powercfg -changename 3ff9831b-6f80-4830-8178-736cd4229e7b 'Revision - Ultra Performance' 'Windows''s Ultimate Performance with optimized settings for newer Intel CPUs.'; powercfg -s 3ff9831b-6f80-4830-8178-736cd4229e7b; powercfg -setacvalueindex scheme_current sub_processor HETEROPOLICY 0; powercfg -setacvalueindex scheme_current sub_processor SCHEDPOLICY 2; powercfg /setactive scheme_current }}};}"
+
+echo Configuring tasks
+schtasks /change /tn "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem" /disable >NUL 2>nul
+schtasks /change /tn "\Microsoft\Windows\MemoryDiagnostic\ProcessMemoryDiagnosticEvents" /disable >NUL 2>nul
+schtasks /change /tn "\Microsoft\Windows\MemoryDiagnostic\RunFullMemoryDiagnostic" /disable >NUL 2>nul
+schtasks /change /tn "\Microsoft\Windows\WindowsUpdate\Scheduled Start" /disable >NUL 2>nul
+schtasks /change /tn "\Microsoft\Windows\Windows Error Reporting\QueueReporting" /disable >NUL 2>nul
+schtasks /change /tn "\Microsoft\Windows\Application Experience\AitAgent" /disable >NUL 2>nul
+schtasks /change /tn "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /disable >NUL 2>nul
+wevtutil sl Microsoft-Windows-SleepStudy/Diagnostic /q:false >NUL 2>nul
+wevtutil sl Microsoft-Windows-Kernel-Processor-Power/Diagnostic /q:false >NUL 2>nul
+wevtutil sl Microsoft-Windows-UserModePowerService/Diagnostic /q:false >NUL 2>nul
+
+echo Configuring boot settings
+bcdedit /deletevalue useplatformclock >NUL 2>nul
+::bcdedit /set useplatformtick yes >NUL 2>nul
+bcdedit /set disabledynamictick yes >NUL 2>nul
+bcdedit /set bootmenupolicy Legacy >NUL 2>nul
+bcdedit /set lastknowngood yes >NUL 2>nul
+
+echo Optimizing NTFS settings
+fsutil behavior set disableLastAccess 1 >NUL 2>nul
+fsutil behavior set disable8dot3 1 >NUL 2>nul
+
+echo Configuring teredo
+netsh interface Teredo set state type=default >NUL 2>nul
+netsh interface Teredo set state servername=default >NUL 2>nul
+
+::https://github.com/meetrevision/playbook/issues/27
+::netsh int tcp set supplemental internet congestionprovider=bbr2 >NUL 2>nul
+
+echo Configuring Windows settings
+net accounts /maxpwage:unlimited
+  
+
+
+  # - !run: {exe: 'PowerShell -NonInteractive -NoLogo -NoProfile -Command "Rename-Computer -NewName $env:computername"', showOutput: false, showError: false}
+PowerShell -NonInteractive -NoLogo -NoProfile -Command "Disable-MMAgent -mc"', showOutput: false, showError: false}
+PowerShell -NonInteractive -NoLogo -NoProfile -Command "Disable-WindowsErrorReporting"', showOutput: false, showError: false}
+powershell -NonInteractive -NoLogo -NoProfile Set-ProcessMitigation -Name vgc.exe -Enable CFG
+  # - !cmd: {exeDir: true, command: '@echo Disable-MMAgent -MC; ForEach($v in (Get-Command -Name "Set-ProcessMitigation").Parameters["Disable"].Attributes.ValidValues){Set-ProcessMitigation -System -Disable $v.ToString().Replace(" ", "").Replace("`n", "")}; rm $PSCommandPath> MC_PM.ps1'}
+  # - !run: {exeDir: true, exe: 'powershell -windowstyle hidden -ExecutionPolicy Bypass -C "& ''./MC_PM.ps1''"'}
+setx DOTNET_CLI_TELEMETRY_OPTOUT 1'}
+setx POWERSHELL_TELEMETRY_OPTOUT 1', weight: 10}
+
+    # OneDrive workaround
+  - !registryValue: {path: 'HKLM\Software\Policies\Microsoft\Windows\OneDrive', value: 'DisableFileSyncNGSC', operation: delete}
+
 
 echo Disabling Superfetch for SSD...
 
