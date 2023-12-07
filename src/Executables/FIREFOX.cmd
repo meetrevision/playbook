5c9f8cef1bb8ed724bff450b96778f5b2ea9516c
h3r0 <50238852+melo936@users.noreply.github.com>
2023-12-07 10:02:25 +0000

    do not add firefox bookmarks

--- src/Executables/FIREFOX.cmd
+++ src/Executables/FIREFOX.cmd
@@ -17,16 +17,16 @@ reg add "HKU\%~1\Software\Policies\Mozilla\Firefox" /v "DisableFirefoxStudies" /
 reg add "HKU\%~1\Software\Policies\Mozilla\Firefox" /v "ExtensionSettings" /t REG_MULTI_SZ /d "{\0    \"uBlock0@raymondhill.net\": {\0        \"installation_mode\": \"force_installed\",\0        \"install_url\": \"https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi\"\0    }\0}" /f >NUL 2>nul
 reg add "HKU\%~1\Software\Policies\Mozilla\Firefox" /v "DisableDefaultBrowserAgent" /t REG_DWORD /d 1 /f >NUL 2>nul
 reg add "HKU\%~1\Software\Policies\Mozilla\Firefox" /v "Preferences" /t REG_MULTI_SZ /d "{\"network.cookie.sameSite.laxByDefault\":{\"Value\":true,\"Status\":\"user\"},\"network.cookie.sameSite.noneRequiresSecure\":{\"Value\":true,\"Status\":\"user\"},\"network.cookie.sameSite.schemeful\":{\"Value\":true,\"Status\":\"user\"},\"browser.contentblocking.category\":{\"Value\":\"strict\",\"Status\":\"user\"},\"browser.newtabpage.activity-stream.showSponsored\":{\"Value\":false,\"Status\":\"user\"},\"browser.newtabpage.activity-stream.showSponsoredTopSites\":{\"Value\":false,\"Status\":\"user\"}}" /f >NUL 2>nul
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "Title" /t REG_SZ /d "Revision" /f >NUL 2>nul
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "URL" /t REG_SZ /d "https://revi.cc/" /f >NUL 2>nul
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "Favicon" /t REG_SZ /d "https://revi.cc/img/favicon.png" /f >NUL 2>nul
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "Placement" /t REG_SZ /d "toolbar" /f >NUL 2>nul
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "Folder" /t REG_SZ /d "" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "Title" /t REG_SZ /d "Revision" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "URL" /t REG_SZ /d "https://revi.cc/" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "Favicon" /t REG_SZ /d "https://revi.cc/img/favicon.png" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "Placement" /t REG_SZ /d "toolbar" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\1" /v "Folder" /t REG_SZ /d "" /f >NUL 2>nul
 
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "Title" /t REG_SZ /d "FAQ" /f >NUL 2>nul
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "URL" /t REG_SZ /d "https://revi.cc/docs/faq/" /f >NUL 2>nul
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "Favicon" /t REG_SZ /d "https://revi.cc/img/favicon.png" /f >NUL 2>nul
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "Placement" /t REG_SZ /d "toolbar" /f >NUL 2>nul
-reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "Folder" /t REG_SZ /d "" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "Title" /t REG_SZ /d "FAQ" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "URL" /t REG_SZ /d "https://revi.cc/docs/faq/" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "Favicon" /t REG_SZ /d "https://revi.cc/img/favicon.png" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "Placement" /t REG_SZ /d "toolbar" /f >NUL 2>nul
+@REM reg add "HKU\%~1\Software\Policies\Mozilla\Firefox\Bookmarks\10" /v "Folder" /t REG_SZ /d "" /f >NUL 2>nul
 
 "%ProgramFiles(x86)%\Mozilla Maintenance Service\uninstall.exe" /S >NUL 2>nul
