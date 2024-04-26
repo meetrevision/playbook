0c7578d6b9ae5f02e3461a48da1c6f6a7aae68e5
h3r0 <50238852+melo936@users.noreply.github.com>
2024-04-26 17:20:03 +0000

    Refactor MS Edge uninstallation

---
+++ src/Executables/EDGE.ps1
@@ -0,0 +1,115 @@
+param (   
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("EdgeBrowser", "WebView", "EdgeUpdate")]
+    [string]$Mode
+)
+
+function Uninstall-Process {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Key
+    )
+    
+    # credits to he3als for the Acl commands
+    $fileName = "IntegratedServicesRegionPolicySet.json"
+    $pathISRPS = [Environment]::SystemDirectory + "\" + $fileName
+    $aclISRPS = Get-Acl -Path $pathISRPS
+    $aclISRPSBackup = [System.Security.AccessControl.FileSecurity]::new()
+    $aclISRPSBackup.SetSecurityDescriptorSddlForm($acl.Sddl)
+    if (Test-Path -Path $pathISRPS) {
+        try {
+            $admin = [System.Security.Principal.NTAccount]$(New-Object System.Security.Principal.SecurityIdentifier('S-1-5-32-544')).Translate([System.Security.Principal.NTAccount]).Value
+        
+            $aclISRPS.SetOwner($admin)
+            $rule = New-Object System.Security.AccessControl.FileSystemAccessRule($admin, 'FullControl', 'Allow')
+            $aclISRPS.AddAccessRule($rule)
+            Set-Acl -Path $pathISRPS -AclObject $aclISRPS
+        
+            Rename-Item -Path $pathISRPS -NewName ($fileName + '.bak') -Force
+        }
+        catch {
+            Write-Error "[$Mode] Failed to set owner for $pathISRPS"
+        }	
+    }
+    
+    $baseKey = 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate'
+    $registryPath = $baseKey + '\ClientState' + $Key
+
+    if (!(Test-Path -Path $registryPath)) {
+        Write-Host "[$Mode] Registry key not found: $registryPath"
+        return
+    }
+
+    Remove-ItemProperty -Path $registryPath -Name "experiment_control_labels" -ErrorAction SilentlyContinue | Out-Null
+
+    $uninstallString = (Get-ItemProperty -Path $registryPath).UninstallString
+    $uninstallArguments = (Get-ItemProperty -Path $registryPath).UninstallArguments
+
+    if ([string]::IsNullOrEmpty($uninstallString) -or [string]::IsNullOrEmpty($uninstallArguments)) {
+        Write-Host "[$Mode] Cannot find uninstall methods for $Mode"
+        return
+    }
+
+    $uninstallArguments += " --force-uninstall --delete-profile"
+
+    # $uninstallCommand = "`"$uninstallString`"" + $uninstallArguments
+    if (!(Test-Path -Path $uninstallString)) {
+        Write-Host "[$Mode] setup.exe not found at: $uninstallString"
+        return
+    }
+    Start-Process -FilePath $uninstallString -ArgumentList $uninstallArguments -Wait -NoNewWindow -Verbose
+
+    # Restore Acl
+    if (Test-Path -Path ($pathISRPS + '.bak')) {
+        Rename-Item -Path ($pathISRPS + '.bak') -NewName $fileName -Force
+        Set-Acl -Path $pathISRPS -AclObject $aclISRPSBackup
+    }
+
+    if ((Get-ItemProperty -Path $baseKey).IsEdgeStableUninstalled -eq 1) {
+        Write-Host "[$Mode] Edge Stable has been successfully uninstalled"
+    }
+}
+
+function Uninstall-Edge {
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge" -Name "NoRemove" -ErrorAction SilentlyContinue | Out-Null
+   
+    # [microsoft.win32.registry]::SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdateDev", "AllowUninstall", 1, [Microsoft.Win32.RegistryValueKind]::DWord) | Out-Null
+
+    Uninstall-Process -Key '{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}'
+
+}
+
+function Uninstall-WebView {
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft EdgeWebView" -Name "NoRemove" -ErrorAction SilentlyContinue | Out-Null
+
+    # Force to use system-wide WebView2 
+    # [microsoft.win32.registry]::SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge\WebView2\BrowserExecutableFolder", "*", "%%SystemRoot%%\System32\Microsoft-Edge-WebView")
+
+    Uninstall-Process -Key '{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}'
+}
+
+function Uninstall-EdgeUpdate {
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge Update" -Name "NoRemove" -ErrorAction SilentlyContinue | Out-Null
+
+    $registryPath = 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate'
+    if (!(Test-Path -Path $registryPath)) {
+        Write-Host "Registry key not found: $registryPath"
+        return
+    }
+    $uninstallCmdLine = (Get-ItemProperty -Path $registryPath).UninstallCmdLine
+
+    if ([string]::IsNullOrEmpty($uninstallCmdLine)) {
+        Write-Host "Cannot find uninstall methods for $Mode"
+        return
+    }
+
+    Write-Output "Uninstalling: $uninstallCmdLine"
+    Start-Process cmd.exe "/c $uninstallCmdLine" -WindowStyle Hidden -Wait
+}
+
+switch ($Mode) {
+    "EdgeBrowser" { Uninstall-Edge }
+    # "WebView" { Uninstall-WebView }
+    # "EdgeUpdate" { Uninstall-EdgeUpdate }
+    default { Write-Host "Invalid mode: $Mode" }
+}
