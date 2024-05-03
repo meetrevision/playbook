8c5be2a4f083f8c0e50327981b9a6640db4d72f0
melo936 <50238852+melo936@users.noreply.github.com>
2024-05-03 14:51:33 +0400

    fix update-appx

--- src/Executables/UPDATE-APPX.ps1
+++ src/Executables/UPDATE-APPX.ps1
@@ -1,8 +1,15 @@
-Add-AppPackage 'https://cdn.winget.microsoft.com/cache/source.msix' -ForceApplicationShutdown
-$productName = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name ProductName
+Write-Host 'Updating Microsoft Store apps...'
+Write-Host 'Installing Winget Source'
+Add-AppPackage 'https://cdn.winget.microsoft.com/cache/source.msix' -ForceApplicationShutdown -Verbose
+$productName = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').ProductName
+Write-Host "Product Name: $productName"
 $path = Join-Path ${env:ProgramFiles(x86)} 'Revision Tool'
-if (Test-Path $path) {
-    $file = Join-Path $path 'revitool.exe'
-    $args = if ($productName -like '*LTSC*') {'msstore-apps --id 9NBLGGH4NNS1 --ring RP'} else {'msstore-apps --id 9WZDNCRFJBMP --id 9NBLGGH4NNS1 --ring RP'}
-    Start-Process -FilePath $file -ArgumentList $args -Wait -WindowStyle Hidden
+$file = Join-Path $path 'revitool.exe'
+
+if (!(Test-Path $path) -or !(Test-Path $file)) {
+    Write-Host 'Revision Tool not found. Skipping update.'
+    exit 1
 }
+
+$argumentsList = if ($productName -like '*LTSC*') {'msstore-apps --id 9NBLGGH4NNS1 --ring RP'} else {'msstore-apps --id 9WZDNCRFJBMP --id 9NBLGGH4NNS1 --ring RP'}
+Start-Process -FilePath $file -ArgumentList $argumentsList -Wait -NoNewWindow -PassThru -Verbose
