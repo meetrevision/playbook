22ecae7cef996597b3148e3f61fc4342f623b9c6
David M <136613722+realize-1337@users.noreply.github.com>
2024-05-15 21:19:37 +0200

    added auto create bat

---
+++ build/run.bat
@@ -0,0 +1,9 @@
+@echo off
+set "source_dir=src"
+set "output_file=custom_windows.apbx"
+set "password=malte"
+
+cd /d "%source_dir%"
+
+REM Compress all files in the directory using 7zip
+7z a -t7z "%output_file%" * -p"%password%"
