fea58170d2747369dcea7d86966e70be3429dfdf
David M <136613722+realize-1337@users.noreply.github.com>
2024-05-15 21:36:19 +0200

    fixed build script

--- build/run.bat
+++ build/run.bat
@@ -6,4 +6,4 @@ set "password=malte"
 cd /d "%source_dir%"
 
 REM Compress all files in the directory using 7zip
-7z a -t7z "%output_file%" * -p"%password%"
+7z a -t7z "%output_file%" * -p"%password%" -aoa
