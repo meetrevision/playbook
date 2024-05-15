164fbf7bf29a27ee2e28015d5e1ef71c1a39a90b
David M <136613722+realize-1337@users.noreply.github.com>
2024-05-15 23:04:44 +0200

    working v1

--- build/run.bat
+++ build/run.bat
@@ -1,9 +1,12 @@
 @echo off
-set "source_dir=src"
-set "output_file=custom_windows.apbx"
+set "script_dir=%~dp0"
+for %%I in ("%script_dir%..") do set "parent_dir=%%~fI"
+
+set "source_dir=%parent_dir%\src\"
+set "output_file=%script_dir%\custom_windows.apbx"
 set "password=malte"
 
 cd /d "%source_dir%"
 
 REM Compress all files in the directory using 7zip
-7z a -t7z "%output_file%" * -p"%password%" -y
+7z a -t7z "%output_file%" * -p"%password%"
