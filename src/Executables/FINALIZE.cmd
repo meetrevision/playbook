9ae475fb3edb0c3a56272a0eb7b88bd8dbb9fa55
h3r0 <50238852+melo936@users.noreply.github.com>
2024-10-12 10:50:06 +0000

    move animations script to final.yml

--- src/Executables/FINALIZE.cmd
+++ src/Executables/FINALIZE.cmd
@@ -103,19 +103,6 @@ echo Configuring animations
 :: Breaks XboxGipSvc
 ::reg add "HKLM\SYSTEM\CurrentControlSet\Control" /v "SvcHostSplitThresholdInKB" /t REG_DWORD /d "4294967295" /f >NUL
 
-for /f "tokens=2 delims==" %%a in ('wmic os get TotalVisibleMemorySize /format:value') do set "mem=%%a"
-
-for /f "usebackq tokens=2 delims=\" %%a in (`reg query "HKEY_USERS" ^| findstr /r /x /c:"HKEY_USERS\\S-.*" /c:"HKEY_USERS\\AME_UserHive_[^_]*"`) do (
-  REM If the "Volatile Environment" key exists, that means it is a proper user. Built in accounts/SIDs do not have this key.
-  reg query "HKEY_USERS\%%a" | findstr /c:"Volatile Environment" /c:"AME_UserHive_" > nul 2>&1
-    if not errorlevel 1 (
-      if %mem% lss 9000000 ( 
-        reg add "HKU\%%a\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "EnableTransparency" /t REG_DWORD /d "0" /f >NUL
-        reg add "HKU\%%a\Control Panel\Desktop\WindowMetrics" /v "MinAnimate" /t REG_SZ /d "0" /f >NUL 2>nul
-      )
-    )
-  )
-
 :: https://github.com/meetrevision/playbook/issues/15
 :: Updates root certificates
 
